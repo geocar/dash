0272ed5829f75022f6d2df926725f432d9e6f091
Geo Carncross <geocar@gmail.com>
2015-11-24 12:14:06 +0000

    fewer syscalls

--- d.c
+++ d.c
@@ -41,7 +41,7 @@ ZI Qq(V){R epoll_create1(0);}ZV Qa(I k,I f){struct epoll_event ev={0};ev.data.fd
 I Qq(V){R kqueue();}ZV Qa(I k,I f){struct kevent ev;EV_SET(&ev,f,EVFILT_READ,EV_ADD|EV_CLEAR,0,NM,NULL);kevent(k,&ev,1,0,0,0);}ZI Qw(I k,V*x,I t){struct timespec tv={0};tv.tv_sec=1;R kevent(k,0,0,x,NM,t>0?&tv:0);}ZI Qf(I k,V*x){struct kevent*e=x,ev;I f;if(e->flags&EV_EOF){EV_SET(&ev,e->ident,EVFILT_READ,EV_DELETE,0,0,0);close(e->ident);kevent(k,&ev,1,0,0,0);f=-1;}else{/*signed*/f=e->ident;}R f;}
 #endif
 
-ZV sc(I f,I b){setsockopt(f,IPPROTO_TCP,TCP_NOPUSH,&b,sizeof(b));} ZV sa(I n){
+ZV sc(I f,I b,I*g){if(*g!=b)*g=b,setsockopt(f,IPPROTO_TCP,TCP_NOPUSH,&b,sizeof(b));} ZV sa(I n){
 #ifdef __APPLE__
 extern int thread_policy_set(thread_t thread, thread_policy_flavor_t flavor, thread_policy_t policy_info, mach_msg_type_number_t count);
 {thread_extended_policy_data_t ep;ep.timeshare=FALSE;thread_policy_set(mach_thread_self(),THREAD_EXTENDED_POLICY,(thread_policy_t)&ep,THREAD_EXTENDED_POLICY_COUNT);
@@ -57,17 +57,17 @@ ZV poop(I f){cwrite(f,"HTTP/1.1 500 problem\r\nConnection:close\r\nContent-Lengt
 #define BLANK "Content-Type: image/gif\r\nContent-Length: 32\r\n\r\nGIF89a\1\0\1\0\0\0\0!\371\4\1\0\0\0\0,\0\0\0\0\1\0\1\0\0\2"
 #define OK(N,K) "HTTP/1.1 "N" ok\r\nConnection: " K "\r\n"
 #define END204 "Content-Length: 0\r\n\r\n"
-ZI http(I d,I f,C*p,I r){I i,o=0,b=0,s=0,w=0,g=0,e=0,c=0,cf=1,m=0;K q=0,a=ktn(11,0),v=ktn(0,0);
+ZI http(I d,I f,C*p,I r,I*sd){I i,o=0,b=0,s=0,w=0,g=0,e=0,c=0,cf=1,m=0;K q=0,a=ktn(11,0),v=ktn(0,0);
   for(i=b=0;i<r;++i){switch(p[i]){
-  case'\n':if(!e)e=i;if(!q){q=kpn(p+w,g-w);if('\r'==(cf=p[i-1]))cf=p[i-2];}else if(!c){w=!!strchr("kK1",cf);if(!o)sc(d,0);else if(w){if('g'==o)cwrite(f,OK("200","keep-alive")BLANK);else cwrite(f,OK("204","keep-alive")END204);}else{if('g'==o)cwrite(f,OK("200","close")BLANK);else cwrite(f,OK("204","close")END204);close(f);}a=k(o?-d:d,"dash",q,xD(a,v),0);b=i+1;if(!o){if(!a){poop(f);R 0;}if(10!=a->t){r0(a);poop(f);R 0;}writer(f,kC(a),a->n);r0(a);if(w)close(f);}if(b==r)R 1;q=0;o=0;a=ktn(11,0),v=ktn(0,0);}else{if((c-m)==10&& !strncasecmp(p+m,"connection",c-m))cf=p[s];js(&a,sn(p+m,c-m));jk(&v,kpn(p+s,e-s));}w=e=g=s=c=0;m=i+1;break;
+  case'\n':if(!e)e=i;if(!q){q=kpn(p+w,g-w);if('\r'==(cf=p[i-1]))cf=p[i-2];}else if(!c){w=!!strchr("kK1",cf);if(!o)sc(d,0,sd);else if(w){if('g'==o)cwrite(f,OK("200","keep-alive")BLANK);else cwrite(f,OK("204","keep-alive")END204);}else{if('g'==o)cwrite(f,OK("200","close")BLANK);else cwrite(f,OK("204","close")END204);close(f);}a=k(o?-d:d,"dash",q,xD(a,v),0);b=i+1;if(!o){if(!a){poop(f);R 0;}if(10!=a->t){r0(a);poop(f);R 0;}writer(f,kC(a),a->n);r0(a);if(w)close(f);}if(b==r)R 1;q=0;o=0;a=ktn(11,0),v=ktn(0,0);}else{if((c-m)==10&& !strncasecmp(p+m,"connection",c-m))cf=p[s];js(&a,sn(p+m,c-m));jk(&v,kpn(p+s,e-s));}w=e=g=s=c=0;m=i+1;break;
   case' ':case'\t':case'\r':if(w&&!g)g=i;if(s==(e=i))++s;break;
   case':':if(!c)s=1+(c=i);break;
   case '/':if(!w)w=i+1;case '?':case '&':if(r>=(i+4)&&p[i+1]=='f'&&p[i+2]=='=')o=p[i+3];default: e=0;break;}}if(a)r0(a),r0(v);if(q)r0(q);R b;}
-ZV*run(I n){I*s=kI(td)+n;C q[QSZ*NM],b[8192];I h,r,f,k,d,c; sa(n+1);d=khpu("127.0.0.1",kdbport,"dash");
+ZV*run(I n){I*s=kI(td)+n;C q[QSZ*NM],b[8192];I h,r,f,k,d,c,sd=69; sa(n+1);d=khpu("127.0.0.1",kdbport,"dash");
   k=1;setsockopt(d,IPPROTO_TCP,TCP_NODELAY,&k,sizeof(k));
-  k=1048576;if(-1==setsockopt(d,SOL_SOCKET,SO_SNDBUF,&k,sizeof(d)))oops("SNDBUF"); sc(d,1);
+  k=1048576;if(-1==setsockopt(d,SOL_SOCKET,SO_SNDBUF,&k,sizeof(d)))oops("SNDBUF"); sc(d,1,&sd);
   pthread_mutex_lock(&tm);k=*s=Qq();pthread_mutex_unlock(&tm);pthread_cond_signal(&tc);//async
-  for(c=-1;;){DO(h=Qw(k,q,c),if(f=Qf(k,q+i*QSZ))if((r=read(f,b,sizeof(b)))==sizeof(b))poop(f);else if(r>0)if(http(d,f,b,r))c=1);if(h<=0){sc(d,0);sc(d,1);c=-1;}}
+  for(c=-1;;){DO(h=Qw(k,q,c),if(f=Qf(k,q+i*QSZ))if((r=read(f,b,sizeof(b)))==sizeof(b))poop(f);else if(r>0)if(http(d,f,b,r,&sd))c=1);if(h<=0){sc(d,0,&sd);sc(d,1,&sd);c=-1;}}
 }
 ZV loop(I s,I t){struct timeval tv={0};I f,r=0;tv.tv_sec=1;for(;;)if(-1!=(f=accept(s,0,0))){setsockopt(f,SOL_SOCKET,SO_RCVTIMEO,&tv,sizeof(tv));fcntl(f,F_SETFL,O_NONBLOCK);Qa(kI(td)[r],f);++r;r=r%t;}}
 ZI busy(I t){DO(t,if(((volatile)kI(td)[t])==-1)R 1);R 0;}
